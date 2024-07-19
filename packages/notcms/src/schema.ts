c668df335deeb8273e3b3bc3116ca31338fa420a
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-07-20 00:07:46 +0900

    feat: infer type :sparkles:

--- packages/notcms/src/schema.ts
+++ packages/notcms/src/schema.ts
@@ -1,6 +1,7 @@
+type Properties = Record<string, 'string' | 'number' | 'date' | 'boolean'>;
 export type PageProperties = Record<
   string,
-  { id: string; properties: Record<string, string> }
+  { id: string; properties: Properties }
 >;
 export const pageProperties = {
   abc: {
@@ -14,7 +15,32 @@ export const pageProperties = {
     id: '1def',
     properties: {
       slug: 'string',
-      category: 'string',
+      category: 'number',
     },
   },
 } satisfies PageProperties;
+
+// Infer the page property types from the pageProperties object
+// e.g. { properties: { slug: 'string', publishedAt: 'date' } } => { slug: string, publishedAt: Date }
+type InferProperties<TProperties extends Properties> = {
+  [Key in keyof TProperties]: TProperties[Key] extends 'string'
+    ? string
+    : TProperties[Key] extends 'number'
+    ? number
+    : TProperties[Key] extends 'date'
+    ? Date
+    : TProperties[Key] extends 'boolean'
+    ? boolean
+    : TProperties[Key] extends 'string[]'
+    ? string[]
+    : TProperties[Key] extends 'number[]'
+    ? number[]
+    : TProperties[Key] extends 'date[]'
+    ? Date[]
+    : TProperties[Key] extends 'boolean[]'
+    ? boolean[]
+    : never;
+};
+const tmp = pageProperties['abc']['properties'];
+type _A = typeof tmp;
+type A = InferProperties<typeof tmp>;
