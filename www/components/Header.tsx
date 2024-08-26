3fa6a52cdc83ad795fb4207bdb01859ceb82edbc
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-08-27 02:01:44 +0900

    update: header with functional links

--- www/components/Header.tsx
+++ www/components/Header.tsx
@@ -1,14 +1,32 @@
+"use client";
 /*
 We're constantly improving the code you see. 
 Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
 */
 
-import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { routes } from "~/src/routes";
+import { siteConfig } from "~/src/site-config";
 
-export const Header = (): JSX.Element => {
+type NavItem = {
+  title: string;
+  href: string;
+};
+const navItems: NavItem[] = [
+  { title: "Home", href: routes.landing },
+  { title: "Pricing", href: routes.pricing },
+  { title: "Templates", href: routes.templates },
+  { title: "Blog", href: routes.blog },
+  { title: "Docs", href: routes.docs },
+];
+export function Header() {
   return (
     <div className="flex w-[1440px] h-16 items-center gap-10 px-32 py-0 relative">
-      <div className="flex items-center gap-1.5 relative flex-1 grow">
+      <Link
+        href={routes.top}
+        className="flex items-center gap-1.5 relative flex-1 grow"
+      >
         <div className="relative w-4 h-3.5 opacity-70">
           <img
             className="absolute w-[17px] h-[17px] -top-px left-0"
@@ -19,26 +37,38 @@ export const Header = (): JSX.Element => {
         <div className="relative w-fit mt-[-1.00px] [text-shadow:0px_1px_2px_#0000000f] [font-family:'Roobert_TRIAL-SemiBold',Helvetica] font-semibold text-white text-xl tracking-[0.60px] leading-[normal] whitespace-nowrap">
           NotCMS
         </div>
-      </div>
-      <div className="relative w-fit [font-family:'Selecta_VF_Trial-Light',Helvetica] font-light text-[#aaafb5] text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap">
-        Pricing
-      </div>
-      <div className="relative w-fit [font-family:'Selecta_VF_Trial-Light',Helvetica] font-light text-[#aaafb5] text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap">
-        Templates
-      </div>
-      <div className="relative w-fit [font-family:'Selecta_VF_Trial-Regular',Helvetica] font-normal text-white text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap">
-        Blog
-      </div>
-      <div className="relative w-fit [font-family:'Selecta_VF_Trial-Light',Helvetica] font-light text-[#aaafb5] text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap">
-        Docs
-      </div>
-      <div className="flex flex-col items-end justify-center gap-2 relative flex-1 grow">
+      </Link>
+      {navItems.map((item) => (
+        <NavItem key={item.title} title={item.title} href={item.href} />
+      ))}
+      <Link
+        href={siteConfig.waitlist}
+        className="flex flex-col items-end justify-center gap-2 relative flex-1 grow"
+      >
         <div className="inline-flex h-7 items-center justify-center gap-2 px-3 py-[9.5px] relative bg-[#e6e6e61a] rounded-[10px]">
           <div className="relative w-fit mt-[-1.00px] [font-family:'Selecta_VF_Trial-Medium',Helvetica] font-medium text-white text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap">
-            Get started
+            {/* Get started */}
+            Join Waitlist
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
-};
+}
+
+function NavItem({ title, href }: { title: string; href: string }) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link href={href}>
+      <div
+        className={`${
+          isActive ? "text-white" : "text-[#aaafb5]"
+        } text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap`}
+      >
+        {title}
+      </div>
+    </Link>
+  );
+}
---
+++ www/src/routes.ts
@@ -0,0 +1,8 @@
+export const routes = {
+  top: "/",
+  landing: "/home",
+  blog: "/blog",
+  pricing: "/pricing",
+  templates: "/templates",
+  docs: "/docs",
+};
