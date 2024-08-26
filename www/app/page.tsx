41e978abd5c25684fc105de8c5285e4c485c3531
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-08-27 02:07:34 +0900

    chore: footer

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Header } from "~/components/Header";
 import { siteConfig } from "~/src/site-config";
 
@@ -6,24 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
-      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          NotCMS
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://x.com/qqpann"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By qqpann
-          </a>
-        </div>
-      </div> */}
 
       <a
-        // waitlist google form
         href={siteConfig.waitlist}
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         target="_blank"
@@ -40,7 +23,16 @@ export default function Home() {
         </p>
       </a>
 
-      <div></div>
+      <div className="w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex pb-4">
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://x.com/qqpann"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coded by qqpann
+        </a>
+      </div>
     </main>
   );
 }
