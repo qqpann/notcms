44a15f2be11dfd632ef913dad65a684c689f67c1
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-10-22 19:49:04 +0900

    chore: tests

--- packages/notcms/test/index.spec.ts
+++ packages/notcms/test/index.spec.ts
@@ -1,24 +1,22 @@
 import { Client } from "../src/client";
 import { schema } from "../src/sample-schema";
 
-describe("index", () => {
-  describe("notcms", () => {
-    it.skip("should successfully list page ids", async () => {
-      const nc = new Client({ schema: schema });
-      const { data } = await nc.query.abc.listPageIds();
-      console.log(data);
-    });
+describe("notcms", () => {
+  it.skip("should successfully list page ids", async () => {
+    const nc = new Client({ schema: schema });
+    const { data } = await nc.query.abc.listPageIds();
+    console.log(data);
+  });
 
-    it.skip("should successfully list pages", async () => {
-      const nc = new Client({ schema: schema });
-      const { data } = await nc.query.abc.listPages();
-      console.log(data);
-    });
+  it.skip("should successfully list pages", async () => {
+    const nc = new Client({ schema: schema });
+    const { data } = await nc.query.abc.listPages();
+    console.log(data);
+  });
 
-    it.skip("should successfully get a page", async () => {
-      const nc = new Client({ schema: schema });
-      const { data } = await nc.query.abc.getPage("123");
-      console.log(data);
-    });
+  it.skip("should successfully get a page", async () => {
+    const nc = new Client({ schema: schema });
+    const { data } = await nc.query.abc.getPage("123");
+    console.log(data);
   });
 });
