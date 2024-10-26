3b735146d56b037516d6bb9a1e2f09b196aeca8c
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-10-27 00:17:36 +0900

    fix: header style

--- www/components/Header.tsx
+++ www/components/Header.tsx
@@ -4,6 +4,7 @@ We're constantly improving the code you see.
 Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
 */
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { routes } from "~/src/routes";
@@ -22,36 +23,36 @@ const navItems: NavItem[] = [
 ];
 export function Header() {
   return (
-    <div className="flex w-[1440px] h-16 items-center gap-10 px-32 py-0 relative">
-      <Link
-        href={routes.top}
-        className="flex items-center gap-1.5 relative flex-1 grow"
-      >
-        <div className="relative w-4 h-3.5 opacity-70">
-          <img
-            className="absolute w-[17px] h-[17px] -top-px left-0"
-            alt="Rectangle"
-            src="/img/notcms-icon.png"
-          />
-        </div>
-        <div className="relative w-fit mt-[-1.00px] [text-shadow:0px_1px_2px_#0000000f] [font-family:'Roobert_TRIAL-SemiBold',Helvetica] font-semibold text-white text-xl tracking-[0.60px] leading-[normal] whitespace-nowrap">
+    <div className="flex max-w-[1440px] mx-auto h-16 items-center px-32 py-0">
+      <Link href={routes.top} className="flex items-center gap-1.5 flex-1 grow">
+        <Image
+          width={17}
+          height={17}
+          alt="NotCMS logo"
+          src="/img/notcms-icon.png"
+        />
+        <div className="w-fit [text-shadow:0px_1px_2px_#0000000f] [font-family:'Roobert_TRIAL-SemiBold',Helvetica] font-semibold text-white text-xl tracking-[0.60px] leading-[normal] whitespace-nowrap">
           NotCMS
         </div>
       </Link>
-      {navItems.map((item) => (
-        <NavItem key={item.title} title={item.title} href={item.href} />
-      ))}
-      <Link
-        href={siteConfig.waitlist}
-        className="flex flex-col items-end justify-center gap-2 relative flex-1 grow"
-      >
-        <div className="inline-flex h-7 items-center justify-center gap-2 px-3 py-[9.5px] relative bg-[#e6e6e61a] rounded-[10px]">
-          <div className="relative w-fit mt-[-1.00px] [font-family:'Selecta_VF_Trial-Medium',Helvetica] font-medium text-white text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap">
+
+      <nav className="flex gap-x-10">
+        {navItems.map((item) => (
+          <NavItem key={item.title} title={item.title} href={item.href} />
+        ))}
+      </nav>
+
+      <div className="flex items-center justify-end gap-2 flex-1 grow">
+        <div className="inline-flex h-7 items-center justify-center gap-2 px-3 py-[9.5px] bg-[#e6e6e61a] rounded-[10px]">
+          <Link
+            href={siteConfig.waitlist}
+            className="w-fit mt-[-1.00px] [font-family:'Selecta_VF_Trial-Medium',Helvetica] font-medium text-white text-sm text-center tracking-[0.14px] leading-[normal] whitespace-nowrap"
+          >
             {/* Get started */}
             Join Waitlist
-          </div>
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
