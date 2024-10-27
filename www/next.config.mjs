6f0c86f451e2ba2c9919abfac0ac4683ee879c6b
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-10-27 20:40:40 +0900

    update: next/image with notcms

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.notcms.com",
+        port: "",
+        pathname: "/v1/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
