663564dce101151d4ccd4ddbb7ddd13923dd186e
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-11-02 10:49:19 +0900

    update: test

--- packages/notcms/test/index.spec.ts
+++ packages/notcms/test/index.spec.ts
@@ -2,21 +2,15 @@ import { Client } from "../src/client";
 import { schema } from "../src/sample-schema";
 
 describe("notcms", () => {
-  it.skip("should successfully list page ids", async () => {
-    const nc = new Client({ schema: schema });
-    const { data } = await nc.query.abc.listPageIds();
-    console.log(data);
-  });
-
   it.skip("should successfully list pages", async () => {
     const nc = new Client({ schema: schema });
-    const { data } = await nc.query.abc.listPages();
+    const [data] = await nc.query.abc.listPages();
     console.log(data);
   });
 
   it.skip("should successfully get a page", async () => {
     const nc = new Client({ schema: schema });
-    const { data } = await nc.query.abc.getPage("123");
+    const [data] = await nc.query.abc.getPage("123");
     console.log(data);
   });
 });
