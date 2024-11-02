4fa485484b6fe71d6fa6b2ff319cff97cce5c615
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-11-02 22:07:59 +0900

    fix: use script for now

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from "~/lib/utils";
 import { siteConfig } from "~/src/site-config";
 
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,18 @@ export default function RootLayout({
         <Footer />
       </body>
       <GoogleAnalytics gaId={siteConfig.gaId} />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${siteConfig.gaId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${siteConfig.gaId}');
+        `}
+      </Script>
     </html>
   );
 }
