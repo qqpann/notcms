9508c596fc09765278e60b8b3a8fef2068d51294
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-11-07 17:36:12 +0900

    chore: matadata

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -14,6 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
+  applicationName: siteConfig.name,
 };
 
 export default function RootLayout({
