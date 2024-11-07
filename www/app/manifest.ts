9d71bee7e2d6607cbc62e9b568dbcb47a54d3203
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-11-07 17:46:09 +0900

    add: favicon

---
+++ www/app/manifest.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next";
+import { siteConfig } from "~/src/site-config";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteConfig.name,
+    short_name: siteConfig.name,
+    description: siteConfig.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000",
+    theme_color: "#000",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
