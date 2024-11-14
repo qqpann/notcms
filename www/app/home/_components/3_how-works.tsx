886469c1454f4634ebd8c9a8394ad8913d6236d0
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-11-15 01:00:31 +0900

    update: width; refactor: code

--- www/app/home/_components/3_how-works.tsx
+++ www/app/home/_components/3_how-works.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import React from "react";
+import { cn } from "~/lib/utils";
 import {
   Section,
   SectionHeader,
@@ -13,91 +15,159 @@ export function HowWorks() {
         <SectionPreTitle>Composable content architecture</SectionPreTitle>
         <SectionTitle>How NotCMS works</SectionTitle>
       </SectionHeader>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center w-full">
         {/* 1 */}
-        <div className="relative -mb-[45px] z-50">
-          <Image
-            src="/img/home/lp-3-how-works-1-website.png"
-            width={256}
-            height={148}
-            alt="Website"
-          />
-          <div className="absolute top-0 right-0 transform translate-x-full pl-8 w-64 text-right">
-            <h3 className="text-xl font-semibold mb-2">Your website</h3>
-            <span className="shrink-0 bg-border h-[1px] w-full" />
-            <p className="text-sm text-gray-400">
+        <Layer className="z-50">
+          <LayerImage>
+            <Image
+              src="/img/home/lp-3-how-works-1-website.png"
+              width={256}
+              height={148}
+              alt="Website"
+            />
+          </LayerImage>
+          <LayerHeader direction="right">
+            <LayerTitle>Your website</LayerTitle>
+            <LayerDescription>
               Powerful and intuitive interfaces that are the fabric of your
               composable content architecture.
-            </p>
-          </div>
-        </div>
+            </LayerDescription>
+          </LayerHeader>
+        </Layer>
         {/* 2 */}
-        <div className="relative -mb-[45px] z-40">
-          <Image
-            src="/img/home/lp-3-how-works-2-codebase.png"
-            width={256}
-            height={148}
-            alt="API"
-          />
-          <div className="absolute top-0 left-0 transform -translate-x-full pr-8 w-64">
-            <h3 className="text-xl font-semibold mb-2">Website codebase</h3>
-            <p className="text-sm text-gray-400">
+        <Layer className="z-40">
+          <LayerImage>
+            <Image
+              src="/img/home/lp-3-how-works-2-codebase.png"
+              width={256}
+              height={148}
+              alt="API"
+            />
+          </LayerImage>
+          <LayerHeader direction="left">
+            <LayerTitle>Website codebase</LayerTitle>
+            <LayerDescription>
               Powerful and intuitive interfaces that are the fabric of your
               composable content architecture.
-            </p>
-          </div>
-        </div>
+            </LayerDescription>
+          </LayerHeader>
+        </Layer>
         {/* 3 */}
-        <div className="relative -mb-[45px] z-30">
-          <Image
-            src="/img/home/lp-3-how-works-3-api.png"
-            width={256}
-            height={148}
-            alt="Dashboard"
-          />
-          <div className="absolute top-0 right-0 transform translate-x-full pl-8 w-64 text-right">
-            <h3 className="text-xl font-semibold mb-2">NotCMS API</h3>
-            <p className="text-sm text-gray-400">
+        <Layer className="z-30">
+          <LayerImage>
+            <Image
+              src="/img/home/lp-3-how-works-3-api.png"
+              width={256}
+              height={148}
+              alt="API"
+            />
+          </LayerImage>
+          <LayerHeader direction="right">
+            <LayerTitle>NotCMS API</LayerTitle>
+            <LayerDescription>
               Powerful and intuitive interfaces that are the fabric of your
               composable content architecture.
-            </p>
-          </div>
-        </div>
+            </LayerDescription>
+          </LayerHeader>
+        </Layer>
         {/* 4 */}
-        <div className="relative -mb-[45px] z-20">
-          <Image
-            src="/img/home/lp-3-how-works-4-dashboard.png"
-            width={256}
-            height={148}
-            alt="NotCMS Dashboard"
-          />
-          <div className="absolute top-0 left-0 transform -translate-x-full pr-8 w-64">
-            <h3 className="text-xl font-semibold mb-2">NotCMS Dashboard</h3>
-            <p className="text-sm text-gray-400">
+        <Layer className="z-20">
+          <LayerImage>
+            <Image
+              src="/img/home/lp-3-how-works-4-dashboard.png"
+              width={256}
+              height={148}
+              alt="NotCMS Dashboard"
+            />
+          </LayerImage>
+          <LayerHeader direction="left">
+            <LayerTitle>NotCMS Dashboard</LayerTitle>
+            <LayerDescription>
               Powerful and intuitive interfaces that are the fabric of your
               composable content architecture.
-            </p>
-          </div>
-        </div>
+            </LayerDescription>
+          </LayerHeader>
+        </Layer>
         {/* 5 */}
-        <div className="relative z-10">
-          <Image
-            src="/img/home/lp-3-how-works-5-editor.png"
-            width={256}
-            height={148}
-            alt="Notion editor/database"
-          />
-          <div className="absolute top-0 right-0 transform translate-x-full pl-8 w-64 text-right">
-            <h3 className="text-xl font-semibold mb-2">
-              Notion editor/database
-            </h3>
-            <p className="text-sm text-gray-400">
+        <Layer className="z-10 mb-0">
+          <LayerImage>
+            <Image
+              src="/img/home/lp-3-how-works-5-editor.png"
+              width={256}
+              height={148}
+              alt="Notion editor/database"
+            />
+          </LayerImage>
+          <LayerHeader direction="right">
+            <LayerTitle>Notion editor/database</LayerTitle>
+            <LayerDescription>
               Powerful and intuitive interfaces that are the fabric of your
               composable content architecture.
-            </p>
-          </div>
-        </div>
+            </LayerDescription>
+          </LayerHeader>
+        </Layer>
       </div>
     </Section>
   );
 }
+
+const Layer = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "relative flex flex-col items-center w-full -mb-[45px]",
+      className
+    )}
+    {...props}
+  />
+));
+Layer.displayName = "Layer";
+
+const LayerImage = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("mx-auto", className)} {...props} />
+));
+LayerImage.displayName = "LayerImage";
+
+interface LayerHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  direction: "left" | "right";
+}
+const LayerHeader = React.forwardRef<HTMLDivElement, LayerHeaderProps>(
+  ({ className, direction, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "absolute top-[calc(50%-28px)] w-64",
+        direction === "right" ? "pr-8 right-0 text-right" : "pl-8 left-0",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+LayerHeader.displayName = "LayerHeader";
+
+const LayerTitle = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn("text-xl font-semibold mb-2", className)}
+    {...props}
+  />
+));
+LayerTitle.displayName = "LayerTitle";
+
+const LayerDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <p ref={ref} className={cn("text-sm text-gray-400", className)} {...props} />
+));
+LayerDescription.displayName = "LayerDescription";
