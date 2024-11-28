33424f59b5fc1fde02d8e31fbe410b2dea2317ca
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-11-29 00:28:30 +0900

    update: footer style 💅

--- www/app/manifest.ts
+++ www/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/img/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/img/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
--- www/components/footer.tsx
+++ www/components/footer.tsx
@@ -1,7 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
-import { siX } from "simple-icons";
 import { routes } from "~/src/routes";
 import { siteConfig } from "~/src/site-config";
+import { Separator } from "./ui/separator";
 
 type Item = {
   title: string;
@@ -11,22 +12,26 @@ const links: Item[] = [
   { title: "Home", href: routes.home },
   { title: "Templates", href: routes.templates },
   { title: "Blog", href: routes.blog },
-  { title: "Docs", href: routes.docs },
 ];
-const explore: Item[] = [
+const socials: Item[] = [
   { title: "X", href: siteConfig.author.x },
-  { title: "GitHub", href: siteConfig.github },
   // { title: "Notion template", href: "/" },
 ];
 const resources: Item[] = [
-  { title: "Terms of use", href: routes.terms },
-  { title: "Privacy policy", href: routes.privacy },
+  { title: "Docs", href: routes.docs },
+  { title: "GitHub", href: siteConfig.github },
   { title: "About", href: routes.about },
 ];
 
 export function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-between h-auto">
+    <footer
+      className="h-auto px-32 pt-16 pb-8 mt-16"
+      style={{
+        background:
+          "linear-gradient(to bottom, rgba(255, 255, 255, 0.08), rgba(255, 255, 255, 0))",
+      }}
+    >
       {/* <div className="w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex pb-4">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -37,29 +42,31 @@ export function Footer() {
           Coded by {siteConfig.author.name} (@{siteConfig.author.handle})
         </a>
       </div> */}
-      <div className="h-[404px] px-32 py-16 justify-center items-start gap-12 inline-flex">
-        <div className="w-[262px] flex-col justify-start items-start gap-6 inline-flex">
-          <div className="justify-start items-center gap-4 inline-flex">
+      <div className="w-full grid grid-cols-6">
+        <div className="col-span-3 flex-col justify-start items-start gap-6 inline-flex">
+          <div className="flex flex-col">
             {/* <siX.svg /> */}
-            <Link href={siteConfig.author.x}>
-              <svg
-                className="size-6 text-white fill-white"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d={siX.path} />
-              </svg>
-            </Link>
+            <Image
+              src="/img/icon-192x192.png"
+              alt="logo"
+              width={64}
+              height={64}
+              className="mb-6"
+            />
+            <span className="text-2xl tracking-tight mb-3">NotCMS</span>
+            <p className="text-sm text-white text-opacity-70">
+              Instantly publish your website content from Notion.
+            </p>
             {/* <div className="w-6 h-6 flex-col justify-center items-center inline-flex" />
             <div className="w-6 h-6 justify-center items-center flex" />
             <div className="w-6 h-6 py-[3px] justify-center items-center flex" />
             <div className="w-6 h-6 justify-center items-center flex" /> */}
           </div>
         </div>
-        <div className="w-[262px] flex-col justify-start items-start gap-3 inline-flex">
-          <div className="self-stretch h-[38px] pb-4 flex-col justify-start items-start gap-2.5 flex">
+        <div className="col-span-1 min-h-[200px] flex-col justify-start items-start gap-3 inline-flex">
+          <div className="flex-col justify-start items-start gap-2.5 flex">
             <div className="self-stretch justify-start items-start inline-flex">
-              <div className="text-white text-base font-semibold font-['Inter'] leading-snug">
+              <div className="text-white text-sm font-semibold font-['Inter'] leading-snug">
                 Links
               </div>
             </div>
@@ -67,35 +74,35 @@ export function Footer() {
           {links.map((link) => (
             <Link
               key={link.title}
-              className="w-full h-[22px] justify-start items-center inline-flex text-white text-base font-normal font-['Inter'] leading-snug"
+              className="w-full h-[22px] justify-start items-center inline-flex text-white text-opacity-70 text-sm font-normal font-['Inter'] leading-snug"
               href={link.href}
             >
               {link.title}
             </Link>
           ))}
         </div>
-        <div className="w-[262px] flex-col justify-start items-start gap-3 inline-flex">
-          <div className="self-stretch h-[38px] pb-4 flex-col justify-start items-start gap-2.5 flex">
+        <div className="col-span-1 min-h-[200px] flex-col justify-start items-start gap-3 inline-flex">
+          <div className="flex-col justify-start items-start gap-2.5 flex">
             <div className="self-stretch justify-start items-start inline-flex">
-              <div className="text-white text-base font-semibold font-['Inter'] leading-snug">
-                Explore
+              <div className="text-white text-sm font-semibold font-['Inter'] leading-snug">
+                Socials
               </div>
             </div>
           </div>
-          {explore.map((link) => (
+          {socials.map((link) => (
             <Link
               key={link.title}
-              className="w-full h-[22px] justify-start items-center inline-flex text-white text-base font-normal font-['Inter'] leading-snug"
+              className="w-full h-[22px] justify-start items-center inline-flex text-white text-opacity-70 text-sm font-normal font-['Inter'] leading-snug"
               href={link.href}
             >
               {link.title}
             </Link>
           ))}
         </div>
-        <div className="w-[262px] flex-col justify-start items-start gap-3 inline-flex">
-          <div className="self-stretch h-[38px] pb-4 flex-col justify-start items-start gap-2.5 flex">
+        <div className="col-span-1 min-h-[200px] flex-col justify-start items-start gap-3 inline-flex">
+          <div className="flex-col justify-start items-start gap-2.5 flex">
             <div className="self-stretch justify-start items-start inline-flex">
-              <div className="text-white text-base font-semibold font-['Inter'] leading-snug">
+              <div className="text-white text-sm font-semibold font-['Inter'] leading-snug">
                 Resources
               </div>
             </div>
@@ -103,13 +110,23 @@ export function Footer() {
           {resources.map((link) => (
             <Link
               key={link.title}
-              className="w-full h-[22px] justify-start items-center inline-flex text-white text-base font-normal font-['Inter'] leading-snug"
+              className="w-full h-[22px] justify-start items-center inline-flex text-white text-opacity-70 text-sm font-normal font-['Inter'] leading-snug"
               href={link.href}
             >
               {link.title}
             </Link>
           ))}
         </div>
+      </div>
+      <Separator className="my-8 opacity-[0.12]" />
+      {/* link to terms and policy */}
+      <div className="w-full flex text-sm gap-8 lg:flex">
+        <Link className="text-white text-opacity-50" href={routes.terms}>
+          Terms of use
+        </Link>
+        <Link className="text-white text-opacity-50" href={routes.privacy}>
+          Privacy policy
+        </Link>
       </div>
     </footer>
   );
