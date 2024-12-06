adcce7a6e4fecf742df671f30959b6ba5ff64949
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-12-07 02:13:14 +0900

    fix: remove colored bg

--- www/app/home/_components/5_make-with.tsx
+++ www/app/home/_components/5_make-with.tsx
@@ -138,7 +138,7 @@ TabsContent.displayName = TabsPrimitive.Content.displayName;
 
 export function MakeWith() {
   return (
-    <Section className="w-full py-12 md:py-24 lg:py-32 bg-zinc-950">
+    <Section className="w-full py-12 md:py-24 lg:py-32">
       {/* <div className="container max-w-6xl px-4 md:px-6"> */}
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
         <div className="flex flex-col h-full justify-between">
