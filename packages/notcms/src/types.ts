faff61c116f3566773587707e41f092389689134
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-12-11 21:45:19 +0900

    update: take actual type

--- packages/notcms/src/types.ts
+++ packages/notcms/src/types.ts
@@ -1,6 +1,32 @@
 export type Properties = Record<
   string,
-  "string" | "number" | "date" | "boolean" | "string[]"
+  // boolean
+  | "checkbox"
+  // number
+  | "number"
+  // string
+  | "select"
+  | "title"
+  | "url"
+  | "created_by"
+  | "last_edited_by"
+  | "email"
+  | "phone_number"
+  | "rich_text"
+  | "status"
+  | "unique_id"
+  | "formula"
+  // string (Date)
+  | "date"
+  | "created_time"
+  | "last_edited_time"
+  // string (unsupported)
+  | "rollup"
+  // string[]
+  | "multi_select"
+  | "files"
+  | "people"
+  | "relation"
 >;
 
 export type Schema = Record<string, { id: string; properties: Properties }>;
@@ -26,13 +52,33 @@ type MapProperties<T extends Properties> = {
     : never;
 };
 type PropertyTypeMap = {
-  string: string | undefined;
-  number: number | undefined;
-  // TODO: convert date string to Date object
-  // date: Date | undefined;
-  date: string | undefined;
-  boolean: boolean | undefined;
-  "string[]": string[] | undefined;
+  // boolean
+  checkbox: boolean | null;
+  // number
+  number: number | null;
+  // string
+  select: string | null;
+  title: string | null;
+  url: string | null;
+  created_by: string | null;
+  last_edited_by: string | null;
+  email: string | null;
+  phone_number: string | null;
+  rich_text: string | null;
+  status: string | null;
+  unique_id: string | null;
+  formula: string | null;
+  //  string (Date)
+  date: string | null;
+  created_time: string | null;
+  last_edited_time: string | null;
+  //  string (unsupported)
+  rollup: string | null;
+  //  string[]
+  multi_select: string[] | null;
+  files: string[] | null;
+  people: string[] | null;
+  relation: string[] | null;
 };
 /** Make the hover overlay more readable */
 type Prettify<T> = {
