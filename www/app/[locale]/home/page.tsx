7594b662cdb308e09c43d81b8f2b75436843de59
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-12-15 07:09:11 +0900

    update: translate metadata

--- www/app/[locale]/home/page.tsx
+++ www/app/[locale]/home/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import { Hero } from "./_components/1_hero";
 import { WhyBetter } from "./_components/2_why-better";
@@ -8,30 +9,46 @@ import { MakeWith } from "./_components/5_make-with";
 import { KillerFeatures } from "./_components/6_killer-features";
 import { LastCatch } from "./_components/last-catch";
 
-export const metadata: Metadata = {
-  title: "NotCMS - Turn Notion into Blog",
-  applicationName: "NotCMS",
-  description:
-    "NotCMS is a Headless CMS that turns Notion into your content database. Say goodbye to legacy or custom bloated CMS, and start writing in Notion today. Deploy your blog with confidence with NotCMS SDK, and grow your audience fast.",
-  keywords: [
-    "Notion",
-    "Blog",
-    "CMS",
-    "Headless CMS",
-    "HCMS",
-    "Content Management System",
-    "WordPress",
-    "Next.js",
-    "React",
-    "TypeScript",
-    "Notion API",
-    "Notion Blog",
-    "Notion to Blog",
-    "Notion as CMS",
-    "Notion as Headless CMS",
-    "Notion as Blog",
-  ],
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  const t = await getTranslations({
+    locale: params.locale,
+    namespace: "home.metadata",
+  });
+
+  return {
+    title: t("title"),
+    applicationName: "NotCMS",
+    description: t("description"),
+    // "NotCMS is a Headless CMS that turns Notion into your content database. Say goodbye to legacy or custom bloated CMS, and start writing in Notion today. Deploy your blog with confidence with NotCMS SDK, and grow your audience fast.",
+    keywords: [
+      "Notion",
+      "Blog",
+      "CMS",
+      "Headless CMS",
+      "HCMS",
+      "Content Management System",
+      "WordPress",
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Notion API",
+      "Notion Blog",
+      "Notion to Blog",
+      "Notion as CMS",
+      "Notion as Headless CMS",
+      "Notion as Blog",
+      "ブログ",
+      "コンテンツ管理システム",
+      "ヘッドレスCMS",
+      "ワードプレス",
+      "ノーション",
+    ],
+  } satisfies Metadata;
+}
 
 export default function Home() {
   return (
