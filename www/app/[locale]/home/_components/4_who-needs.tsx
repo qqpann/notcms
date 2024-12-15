001a1c5f86fef9eb19d3524f1041dd4dd5505843
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2024-12-15 16:54:34 +0900

    update: icons

--- www/app/[locale]/home/_components/4_who-needs.tsx
+++ www/app/[locale]/home/_components/4_who-needs.tsx
@@ -19,32 +19,32 @@ export function WhoNeeds() {
   }[] = [
     // 1st row
     {
-      iconUrl: "/img/home/icon-people.svg",
+      iconUrl: "/img/home/icon-independent-bloggers.svg",
       title: "Independent bloggers",
       description:
         "Focus on writing and growing your audience. No more worrying, with the quickstart templates.",
     },
     {
-      iconUrl: "/img/home/icon-pencil2.svg",
+      iconUrl: "/img/home/icon-content-writers.svg",
       title: "Content writers",
       description:
         "Write content in a familiar interface and see it go live instantly. No more waiting or back-and-forth.",
     },
     {
-      iconUrl: "/img/home/icon-people.svg",
+      iconUrl: "/img/home/icon-devs-engineers.svg",
       title: "Developers & engineers",
       description:
         "Write code faster and confidently with a type safe SDK. No more hassle syncing types.",
     },
     // 2nd row
     {
-      iconUrl: "/img/home/icon-people.svg",
+      iconUrl: "/img/home/icon-business-managers.svg",
       title: "Business managers",
       description:
         "Get a bird's eye view of your content and make data-driven decisions. No more guesswork.",
     },
     {
-      iconUrl: "/img/home/icon-people2-group.svg",
+      iconUrl: "/img/home/icon-small-teams.svg",
       title: "Small teams",
       description:
         "Manage your content and collaborate with your team effortlessly. No more silos.",
@@ -67,8 +67,8 @@ export function WhoNeeds() {
               <Image
                 src={userGroups[0].iconUrl}
                 alt={userGroups[0].title}
-                width={16}
-                height={16}
+                width={40}
+                height={40}
               />
             }
             title={userGroups[0].title}
@@ -83,8 +83,8 @@ export function WhoNeeds() {
               <Image
                 src={userGroups[1].iconUrl}
                 alt={userGroups[1].title}
-                width={16}
-                height={16}
+                width={40}
+                height={40}
               />
             }
             title={userGroups[1].title}
@@ -99,8 +99,8 @@ export function WhoNeeds() {
               <Image
                 src={userGroups[2].iconUrl}
                 alt={userGroups[2].title}
-                width={16}
-                height={16}
+                width={40}
+                height={40}
               />
             }
             title={userGroups[2].title}
@@ -116,8 +116,8 @@ export function WhoNeeds() {
               <Image
                 src={userGroups[3].iconUrl}
                 alt={userGroups[3].title}
-                width={16}
-                height={16}
+                width={40}
+                height={40}
               />
             }
             title={userGroups[3].title}
@@ -132,8 +132,8 @@ export function WhoNeeds() {
               <Image
                 src={userGroups[4].iconUrl}
                 alt={userGroups[4].title}
-                width={16}
-                height={16}
+                width={40}
+                height={40}
               />
             }
             title={userGroups[4].title}
@@ -153,9 +153,7 @@ type UserGroupProps = {
 function UserGroup({ icon, title, description }: UserGroupProps) {
   return (
     <div className="flex flex-col items-start gap-5 pt-6 pb-7 px-6 relative flex-1 grow">
-      <div className="inline-flex items-center gap-2 p-3 relative flex-[0_0_auto] bg-[#ffffff03] rounded-full overflow-hidden border-[0.5px] border-white border-opacity-10 shadow-[inset_0px_-12px_16px_#ffffff14]">
-        {icon}
-      </div>
+      {icon}
 
       <div className="flex-col gap-4 flex items-start relative self-stretch w-full flex-[0_0_auto]">
         <div className="relative self-stretch mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-white text-xl tracking-[0.20px] leading-5">
