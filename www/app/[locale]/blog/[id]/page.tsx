0d0876ca661b0b176a9645912b616b7059066fc1
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2025-01-12 00:45:42 +0900

    wip: ISR

--- www/app/[locale]/blog/[id]/page.tsx
+++ www/app/[locale]/blog/[id]/page.tsx
@@ -3,6 +3,17 @@ import { notFound } from "next/navigation";
 import { BlogPageDetail } from "~/components/blog/detail";
 import { nc } from "~/src/notcms/schema";
 
+// ISR: https://nextjs.org/docs/app/building-your-application/data-fetching/incremental-static-regeneration
+
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 60 seconds.
+export const revalidate = 60;
+
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true; // or false, to 404 on unknown paths
+
 interface PageProps {
   params: { id: string };
 }
