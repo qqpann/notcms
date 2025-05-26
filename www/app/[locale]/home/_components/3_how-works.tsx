08df1ad8368fdb2ab526c90d3d922bff973a8b5a
Qiushi Pan <17402261+qqpann@users.noreply.github.com>
2025-05-26 10:33:49 +0900

    add: two layers icon for section 3

--- www/app/[locale]/home/_components/3_how-works.tsx
+++ www/app/[locale]/home/_components/3_how-works.tsx
@@ -13,6 +13,21 @@ import {
   SectionTitle,
 } from "./_section";
 
+const TwoLayersIcon = () => (
+  <svg
+    width="13"
+    height="13"
+    viewBox="0 0 13 13"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10.9229 7.10168C11.6716 7.46693 11.6716 8.53331 10.9229 8.89856L6.93848 10.8429C6.66183 10.9777 6.33814 10.9778 6.06152 10.8429L2.07715 8.89856C1.32863 8.53326 1.32866 7.46701 2.07715 7.10168L2.28516 7.00012L6.06152 8.8429C6.33814 8.9778 6.66183 8.97772 6.93848 8.8429L10.7158 7.00012L10.9229 7.10168ZM6.06152 2.15735C6.33814 2.02241 6.66183 2.0225 6.93848 2.15735L10.9229 4.10168C11.6716 4.46694 11.6716 5.53331 10.9229 5.89856L6.93848 7.8429C6.66183 7.97773 6.33814 7.97781 6.06152 7.8429L2.07715 5.89856C1.32864 5.53325 1.32865 4.46699 2.07715 4.10168L6.06152 2.15735Z"
+      fill="#CCCCCC"
+    />
+  </svg>
+);
+
 export function HowWorks() {
   const ref = React.useRef<HTMLDivElement>(null);
   React.useEffect(() => {
@@ -30,7 +45,10 @@ export function HowWorks() {
       }}
     >
       <SectionHeader>
-        <SectionPreTitle>How it works</SectionPreTitle>
+        <SectionPreTitle>
+          <TwoLayersIcon />
+          Architecture
+        </SectionPreTitle>
         <SectionTitle>
           Complete customization
           <br />
